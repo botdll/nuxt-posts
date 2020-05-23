2f7615da534f69daba65fbcb839ed93d3e4dbde3
George Tsilikas <tsili.g@gmail.com>
2020-05-23 18:10:26 +0300

    vuex integration

--- pages/index.vue
+++ pages/index.vue
@@ -42,10 +42,7 @@ import PostItem from '@/components/PostItem'
     data() {
       return {
         title: 'My title from page',
-        posts: [
-          {_id: '1', title: 'My first post', subtitle: 'My first subtitle', createdAt: new Date(), isRead: false},
-          {_id: '2', title: 'My second post', subtitle: 'My second subtitle', createdAt : new Date(), isRead: true}
-        ]
+        posts: this.$store.state.posts
       }
     }
   }
